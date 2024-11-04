e59a752af5827e8bba17f24313b0a230aa165e08
Shahidul Islam <shahidul1920shakil@gmail.com>
2024-11-05 00:56:45 +0600

    server working on upload product

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 const port = 3000
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
 
 
 //middleware
-
+app.use(cors());
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.MONGO_userID}:${process.env.MONGO_pass}@cluster0.kfgpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -19,14 +21,29 @@ const client = new MongoClient(uri, {
   }
 });
 
-const user = {name: "shakil", email: "shakil@basmaki.com"}
-
 async function run() {
   try {
     await client.connect();
     const database = client.db("ProductsOfBC")
     const userCollection = database.collection("Users");
-    userCollection.insertOne(user);
+    const productCollection = database.collection("Products");
+    //userCollection.insertOne(user);
+    //productCollection.insertOne(user);
+
+    //getting product
+
+    app.get("/products", async(req, res)=>{
+      const query = {};
+      const allProducts = await productCollection.find(query).toArray();
+      res.send(allProducts)
+    })
+
+    //post product
+    app.post("/products", async(req, res)=>{
+      const upcomingProd = req.body;
+      const result = await productCollection.insertOne(upcomingProd);
+      res.send(result);
+    })
 
      
   } finally {
